8c652a12b5b692d49eecda6a0d14c7adccaf97ed
jek <wiramatajaya10@gmail.com>
2026-04-12 20:10:14 +0700

    js

---
+++ js/2javascript/eksplorasi.js
@@ -0,0 +1,9 @@
+
+const judulH1 = document.getElementById('title');
+judulH1.textContent = "Judul Telah Berhasil Diubah!";
+
+
+const linkTombol = document.querySelector('#my-link');
+linkTombol.setAttribute('href', 'https://github.com');
+linkTombol.textContent = "Kunjungi GitHub Saya";
+
